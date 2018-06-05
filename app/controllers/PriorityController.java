024382be3b637a79175dfaca10e5b99b19ea0e8d
tjheckler <timothyheckler@gmail.com>
2018-06-05 07:03:50 -0500

    Priorities Functional

--- app/controllers/PriorityController.java
+++ app/controllers/PriorityController.java
@@ -1,32 +1,94 @@
 package controllers;
 
+
         import models.Priority;
+        import play.data.DynamicForm;
         import play.data.FormFactory;
+        import play.db.jpa.JPAApi;
+        import play.db.jpa.Transactional;
         import play.mvc.Result;
 
         import javax.inject.Inject;
 
+        import java.util.List;
+
         import static play.mvc.Results.ok;
+        import static play.mvc.Results.redirect;
 
 public class PriorityController
 {
+    private JPAApi jpaApi;
     private FormFactory formFactory;
-
     @Inject
-    public PriorityController(FormFactory formFactory)
+    public PriorityController(JPAApi jpaApi,FormFactory formFactory)
     {
-        this.formFactory=formFactory;
+        this.jpaApi = jpaApi;
+        this.formFactory = formFactory;
     }
-
+    @Transactional(readOnly = true)
     public Result getPriorities()
     {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        String sql = "SELECT p FROM Priority p " +
+                " WHERE priorityName LIKE :searchCriteria " +
+                "ORDER BY priorityName";
+        String searchCriteria = form.get("searchCriteria");
+        if (searchCriteria == null)
+        {
+            searchCriteria = "";
+        }
+        String queryParameter = searchCriteria + "%";
 
-        return ok();
+        List<Priority> priorities = jpaApi.em()
+                .createQuery(sql,Priority.class).setParameter("searchCriteria", queryParameter).getResultList();
 
+
+        return ok(views.html.Priority.priorityList.render(priorities, searchCriteria));
     }
-    public Result getPriority(int id)
+    @Transactional(readOnly = true)
+    public Result getPriority(Integer priorityId)
     {
+        String sql = "SELECT p FROM Priority p " +
+                "WHERE priorityId = :priorityId";
+        Priority priority = jpaApi.em().createQuery(sql, Priority.class).
+                setParameter("priorityId", priorityId).getSingleResult();
 
-        return ok();
+        return ok(views.html.Priority.priority.render(priority));
+    }
+
+    @Transactional
+    public Result postPriority(Integer priorityId)
+    {
+        String sql = "SELECT p FROM Priority p " +
+                "WHERE priorityId = :categoryId";
+
+        Priority priority = jpaApi.em().createQuery(sql, Priority.class)
+                .setParameter("priorityId", priorityId).getSingleResult();
+        DynamicForm form = formFactory.form().bindFromRequest();
+
+        String priorityName = form.get("priorityName");
+        priority.setPriorityName(priorityName);
+
+
+        jpaApi.em().persist(priority);
+
+        return redirect(routes.PriorityController.getPriorities());
+    }
+    public Result getNewPriority()
+    {
+        return ok(views.html.Priority.newpriority.render());
+    }
+
+    @Transactional
+    public Result postNewPriority()
+    {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        String priority1 = form.get("priority");
+        Priority priority = new Priority();
+        priority.setPriorityName(priority1);
+        jpaApi.em().persist(priority);
+
+
+        return redirect(routes.PriorityController.getPriorities());
     }
 }
