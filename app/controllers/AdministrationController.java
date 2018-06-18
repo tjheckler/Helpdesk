2fa77b862c045e57fe7dfd3b5d9a27204e873b9f
tjheckler <timothyheckler@gmail.com>
2018-06-18 14:42:58 -0500

    Added Login Code

--- app/controllers/AdministrationController.java
+++ app/controllers/AdministrationController.java
@@ -1,20 +1,67 @@
 package controllers;
 
+import javafx.scene.control.Alert;
+import models.SiteAdmin;
+import models.Ticket;
+import play.data.DynamicForm;
+import play.data.FormFactory;
+import play.db.jpa.JPAApi;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+import java.util.List;
+
 public class AdministrationController extends Controller
 {
+    private JPAApi jpaApi;
+    private FormFactory formFactory;
+
+    @Inject
+    public AdministrationController(JPAApi jpaApi, FormFactory formFactory)
+    {
+        this.jpaApi = jpaApi;
+        this.formFactory = formFactory;
+    }
+
+
     public Result getAdministration()
     {
+
         return ok(views.html.Administration.admin.render());
     }
 
-    public Result getSignUp() {
+    public Result getSignUp()
+    {
         return ok(views.html.Administration.signup.render());
     }
 
-    public Result getLogin() {
-        return ok(views.html.Administration.login.render());
+    @Transactional(readOnly = true)
+    public Result getLogin()
+    {
+
+
+        return ok(views.html.Administration.login.render(""));
+    }
+
+    @Transactional
+    public Result postLogin()
+    {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        String username = form.get("username");
+        String password = form.get("password");
+
+        String sql = "SELECT sa FROM SiteAdmin sa WHERE userName = :username AND password = :password";
+
+        List<SiteAdmin> siteAdmins = jpaApi.em()
+                .createQuery(sql, SiteAdmin.class).setParameter("username", username).setParameter("password", password).getResultList();
+        if (siteAdmins.size() == 0)
+        {
+            return ok(views.html.Administration.login.render("Invalid Username or Password"));
+        } else
+        {
+            return redirect(routes.TicketController.getTickets());
+        }
     }
 }
